95ee61050be93f58827e6c96b12992de0410a587
Patrick McMorran <kingrolloinc@sbcglobal.net>
2017-10-11 23:59:52 -0400

    updated R commands with Parens

--- libraries/default.rb
+++ libraries/default.rb
@@ -2,7 +2,7 @@
 def r_package_installed?(package)
   require 'rinruby'
   R.echo(enable: false)
-  R.eval 'packages = installed.packages()[,1]'
-  packages = R.pull 'packages'
+  R.eval('packages = installed.packages()[,1]')
+  packages = R.pull('packages')
   packages.include?(package)
 end
